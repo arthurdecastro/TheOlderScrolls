a5a1c26161bb7f82c6d933da6d28dca8d7956f7c
arthurdecastro <arthurdecastro666@gmail.com>
2017-04-18 23:21:40 -0300

    Last commit of the day. Really.

---
+++ tos1.bat
@@ -0,0 +1,30 @@
+@echo off
+cls
+color 0f
+echo -------------------------------------------------------------------------------
+echo.
+echo  The Older Scrolls 1 - Duiaboel (By "Arthur's")
+echo.
+echo  version 1.0 - 2015/Dec/02
+echo.
+echo  ***   Will add a Github link here on next update   ***
+echo.
+echo -------------------------------------------------------------------------------
+echo.
+echo  Controls: W = forward  S = Backward  A = turn left  D = Turn Right
+echo.
+echo  	Q = "yes"  E = "no"  R = newgame  ESC = exit P = debug mode
+echo.
+echo  Debug controls: left / righ arrow = fov change
+echo.
+echo  	up / down arrow = change actual dialogue option 
+echo. 
+echo -------------------------------------------------------------------------------
+echo.
+echo  Code by Arthur de Castro ("Arthurion")
+echo.
+echo  Art by Arthur Schunemann (and some material from others that will be removed)
+echo.
+pause
+cd Debug
+ProjectOGL.exe
